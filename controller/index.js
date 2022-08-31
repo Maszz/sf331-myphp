80af20d9944eb7402ff0f3f0cf81caaec6ae5b05
Masz <mawin38408@gmail.com>
2022-08-31 20:35:17 +0700

    init

--- controller/index.js
+++ controller/index.js
@@ -4,8 +4,11 @@ const router = express.Router();
 const process = require("process");
 const user = require("../db/user.model.js");
 router.get("/", (req, res) => {
+  /**
+   * Query ข้อมูลตรงนี้เเล้วใส่เเทน `testName`
+   */
   res.render(path.join(process.cwd(), "views", "index.html"), {
-    name: "testName",
+    data: "testName",
   });
 });
 module.exports = router;
